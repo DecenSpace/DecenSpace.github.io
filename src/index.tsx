c3da17bbd2c49eb09f64755c75b320e0f4fc0a93
Andreas Neumann <andy@neumann-home.net>
2025-03-10 00:44:11 +0100

    add real texts

--- src/index.tsx
+++ src/index.tsx
@@ -13,14 +13,22 @@ createRoot(document.body).render(
             <Box margin={12} position="relative">
                 <SvgIcon component={SvgDsLogo} color="text.primary" height={120} />
                 <Typography variant="h1" sx={{ marginTop: 4 }}>Hello space</Typography>
-                <Typography variant="h2">Headline 2</Typography>
-                <Typography variant="h3">Headline 3</Typography>
-                <Typography variant="h4">Headline 4</Typography>
-                <Typography variant="body1">body 1</Typography>
+                <Typography variant="h2">
+                    Decentralized space communication
+                    </Typography>
+                <Typography variant="h3">
+                    Headline 3
+                </Typography>
+                <Typography variant="h4">
+                    Bringing satellite companies and ground station operators together using blockchain technology.
+                    Our vision is to provide a protocol for a sustainable and accelerating marketplace for the space industry, where all parties benefit from commitment and long term participation and growth.</Typography>
+                <Typography variant="body1">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                </Typography>
+                <Button marginTop={4}>Button</Button>
                 <Box position="absolute" top={0} right={0}>
                     <Box component="img" src="/assets/dish_vertical.jpg" alt="placeholder" width={250} />
                 </Box>
-            <Button>Button</Button>
             </Box>
         </ThemeProvider>
     </StrictMode>
