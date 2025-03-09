3f1b4ac237748289300d6168df5f88c4bc1c65e6
Andreas Neumann <andy@neumann-home.net>
2025-03-10 00:44:05 +0100

    fix button, improve button visibility

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,31 +1,33 @@
-import { blend, Box, BoxProps, darken, styled } from "@mui/system";
+import { blend, Box, BoxProps, lighten, styled } from "@mui/system";
 
 const disableGray = "#7782A1";
 
 const ButtonBase = styled(Box)<{ disabled?: boolean }>(({ theme, disabled }) => ({
-        fontSize: 20,
-        lineHeight: "1.2",
-        padding: theme.spacing(2.5, 4),
-        display: "inline-flex",
-        borderRadius: 36,
-        color: theme.palette.primary.contrastText,
-        backgroundColor: disabled ? blend(theme.palette.primary.main, disableGray, 0.5) : theme.palette.primary.main,
-        cursor: "pointer",
-        transition: "background-color 0.2s",
-        border: 0,
-        "&:hover": {
-            backgroundColor: disabled ?
-                blend(theme.palette.primary.main, disableGray, 0.5) :
-                darken(theme.palette.primary.main, 0.1)
-        }
-}));
+    fontSize: 20,
+    lineHeight: "1.2",
+    letterSpacing: "0.04em",
+    padding: theme.spacing(2.5, 4),
+    display: "inline-flex",
+    borderRadius: 36,
+    color: theme.palette.primary.contrastText,
+    backgroundColor: disabled ? blend(theme.palette.primary.main, disableGray, 0.5) : theme.palette.primary.main,
+    cursor: "pointer",
+    transition: "background-color 0.2s",
+    border: 0,
+    "&:hover": {
+        backgroundColor: disabled ?
+            blend(theme.palette.primary.main, disableGray, 0.5) :
+            lighten(theme.palette.primary.main, 0.1)
+    },
+    boxShadow: "0 8px 16px rgba(0, 0, 0, 0.15)"
+})) as typeof Box;
 
 /**
  * Button component, just like Material UI's Button component, but based directly on @mui/system alone.
  * May remove this and use Material UI's Button component directly if you're using Material UI.
 */
 const Button: React.FC<React.HTMLProps<"button"> & BoxProps> = ({ children, ...props }) => (
-    <ButtonBase {...props} as="button">{children}</ButtonBase>
+    <ButtonBase component="button" {...props}>{children}</ButtonBase>
 );
 
 export default Button;
