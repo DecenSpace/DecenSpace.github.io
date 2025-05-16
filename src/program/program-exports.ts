c619362e2dfaae7882a601ff9b9080cc44d89b82
idkimbc <idkimbc@gmail.com>
2025-05-16 23:02:27 +0200

    program bug fix

--- src/program/program-exports.ts
+++ src/program/program-exports.ts
@@ -2,7 +2,7 @@ import { PublicKey } from "@solana/web3.js";
 import { IDL, type SatelliteMint } from "./satellite-mint-program";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
 
-const programId = new PublicKey("CpucT6fFSSG3DCyjMYi16NndjMgp1SL1iSyspFLnnkTT");
+const programId = new PublicKey("FZQmSamSJdtB9JKxbUH82ZdRQ2UcqqBPGbyce2ZdfviN");
 
 export function getSatelliteProgram(provider: AnchorProvider) {
     const program = new Program<SatelliteMint>(IDL, programId, provider);
